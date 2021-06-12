2a44e9f52f2f1c27082f57e100ea195d01736dbd
Sebastian Beltran <beltran.sebastiang@gmail.com>
2021-06-12 11:49:45 -0500

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,22 +1,56 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+
+bands.addBand(new Band( 'Queen'));
+bands.addBand(new Band( 'Bon Jovi'));
+bands.addBand(new Band( 'Heroes del silencio'));
+bands.addBand(new Band( 'Metallica'));
 
 //Mensajes de Sockets
 
 io.on('connection', client => {
     console.log('Cliente conectado');
 
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
      });
 
-
-    //  client.emit('mensaje', (payload) => {
-    //      console.log('mensaje!!!', payload);
-    //  });
      client.on('mensaje', ( payload ) => {
         console.log('Mensaje', payload);
 
         io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+
+    });
+
+    client.on('emitir-mensaje', (payload) => {
+        console.log(payload);
+        // io.emit('nuevo-mensaje', payload); //emite a todos!
+        client.broadcast.emit('nuevo-mensaje', payload); //a todos menos al que lo emitio
+    });
+
+    client.on('vote-band', (payload) => {
+        // console.log(payload);
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+
+    });
+    //Escuchar el evento add-band y notificar a todos los usuarios el getBands
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        // console.log(payload);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+    client.on('delete-band', (payload) => {
+        // console.log(payload);
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
 
     });
 }); 
